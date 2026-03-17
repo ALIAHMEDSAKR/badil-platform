74999974122499ea60d5bf72cc49cddb3c54c395
Ali Sakr <aliahmedtv911@gmail.com>
2026-03-17 09:45:17 +0200

    chore: Refine clean architecture skeleton with ef configs

--- Badil.API/Program.cs
+++ Badil.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Badil.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Badil.API
 {
     public class Program
@@ -13,6 +16,10 @@ namespace Badil.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>( options => // Setup The Connection String for AppDbContext
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+                );
 
             var app = builder.Build();
 
