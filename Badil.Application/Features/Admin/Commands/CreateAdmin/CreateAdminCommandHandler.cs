99c8db508ed28c75f5d396557f959158a01444f1
Boshra <215034432+boshra27@users.noreply.github.com>
2026-04-26 01:52:37 +0300

    feat: add admin lifecycle (commands, queries, handlers)

---
+++ Badil.Application/Features/Admin/Commands/CreateAdmin/CreateAdminCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using Badil.Application.Common.Interfaces.Repositories;
+using Badil.Domain.Entity;
+using Badil.Domain.Enum;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badil.Application.Features.Admin.Commands.CreateAdmin
+{
+    public class CreateAdminCommandHandler
+    : IRequestHandler<CreateAdminCommand, Guid>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public CreateAdminCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public async Task<Guid> Handle(CreateAdminCommand request, CancellationToken cancellationToken)
+        {
+            var user = new AppUser
+            {
+                UserName = request.Email,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Role = UserRole.Admin,
+                IsActive = true
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded)
+                throw new Exception(string.Join(", ", result.Errors.Select(e => e.Description)));
+
+            return user.Id;
+        }
+    }
+}
---
+++ Badil.Application/Features/Admin/Commands/UpdateUserRole/UpdateUserRoleCommand.cs
@@ -0,0 +1,16 @@
+﻿using Badil.Domain.Enum;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badil.Application.Features.Admin.Commands.UpdateUserRole
+{
+    public class UpdateUserRoleCommand : IRequest
+    {
+        public Guid UserId { get; set; }
+        public UserRole NewRole { get; set; }
+    }
+}
